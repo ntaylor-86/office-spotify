3a323fec9bb7cfdb79f64c06ed7586c8be1e316c
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-24 07:57:24 +1000

    wip

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -31,15 +31,37 @@ defineProps({
     <div
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:pt-0"
     >
+
         <div class="bg-white px-8 py-6 shadow-md mt-10 rounded-lg" style="width: 490px;">
 
             <div class="grid grid-cols-1 text-center">
-                <div class="text-gray-600 font-bold tracking-widest">
-                    🎵 NOW PLAYING 🎵
+                <div v-if="currentTrack === null" class="text-gray-600 font-bold tracking-widest">
+                    ⏸️ PAUSED ⏸️
+                </div>
+                <div v-else class="text-gray-600 font-bold tracking-widest">
+                    🎵 NOW PLAYING 🎵 
                 </div>
             </div>
 
-            <div class="mt-10">
+            <!-- Cover -->
+            <div v-if="currentTrack === null" class="mt-10">
+                <div class="relative">
+                    <img
+                        class="relative rounded drop-shadow-md" 
+                        style="z-index: 10; width: 300px;"
+                        src="images/default_cover.png" 
+                        alt="cover" 
+                    >
+                    <img 
+                        class="absolute top-0 z-0"
+                        style="width: 298px; top: 1px; left: 125px; z-index: 0;"
+                        src="images/CD.png"
+                        alt="cd"
+                    >
+                </div>
+            </div>
+
+            <div v-else class="mt-10">
                 <div class="relative">
                     <img 
                         class="relative rounded drop-shadow-md"
@@ -49,19 +71,23 @@ defineProps({
                     >
                     <img 
                         class="absolute disk top-0 z-0"
-                        style="width: 300px; left: 115px; z-index: 0;"
+                        style="width: 298px; top: 1px; left: 125px; z-index: 0;"
                         src="images/CD.png"
                         alt="cd"
                     >
                 </div>
             </div>
 
+            <!-- Track Info -->
             <div class="mt-5">
                 <div>
                     <span class="text-xs tracking-wide text-emerald-400">
                         Artist:
                     </span>
-                    <span class="tracking-wide text-gray-400">
+                    <span v-if="currentTrack === null" class="tracking-wide text-gray-400">
+                        
+                    </span>
+                    <span v-else class="tracking-wide text-gray-400">
                         {{ currentTrack.artist }}
                     </span>
                 </div>
@@ -69,7 +95,10 @@ defineProps({
                     <span class="text-xs tracking-wide text-emerald-400">
                         Track:
                     </span>
-                    <span class="tracking-wide text-gray-400">
+                    <span v-if="currentTrack === null" class="tracking-wide text-gray-400">
+                        
+                    </span>
+                    <span v-else class="tracking-wide text-gray-400">
                         {{ currentTrack.track }}
                     </span>
                 </div>
@@ -77,37 +106,42 @@ defineProps({
                     <span class="text-xs tracking-wide text-emerald-400">
                         Release Date:
                     </span>
-                    <span class="tracking-wide text-gray-400">
+                    <span v-if="currentTrack === null" class="tracking-wide text-gray-400">
+                        
+                    </span>
+                    <span v-else class="tracking-wide text-gray-400">
                         {{ currentTrack.releaseDate }}
                     </span>
                 </div>
             </div>
 
-            <hr class="mt-10">
+            <!-- Vote For This Track -->
+            <div v-if="currentTrack !== null">
+                <hr class="mt-10">
 
-            <div class="mt-5 grid grid-cols-1 text-center">
-                <div class="text-indigo-600 tracking-widest">
-                    VOTE FOR THIS TRACK
+                <div class="mt-5 grid grid-cols-1">
+                    <div class="text-indigo-600 tracking-widest">
+                        VOTE FOR THIS TRACK
+                    </div>
                 </div>
-            </div>
 
-            <div class="mt-4 grid grid-cols-2 gap-4">
-                <div class="text-right text-2xl">
+                <div class="mt-4 grid grid-cols-2 gap-4">
+                </div>
+
+                <div class="">
                     <button 
                         type="button" 
                         class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
                     >
                         Yeah
-                        👍
+                        <img src="images/+1.png" alt="yeah" class="w-6 inline">
                     </button>
-                </div>
-                <div class="text-left text-2xl">
                     <button 
                         type="button" 
                         class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
                     >
                         Nah
-                        👎
+                        <img src="images/-1.png" alt="nah" class="w-6 inline">
                     </button>
                 </div>
             </div>
