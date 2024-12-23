5e01ce097633d998ed8cec9eb7269e5f104443e8
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-23 21:18:47 +1000

    Update Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,15 @@
 <script setup>
+import { ref } from 'vue';
 import { Head, Link } from '@inertiajs/vue3';
+
+const loading = ref(false);
+const nowPlaying = ref({
+    uri: 'spotify:track:78FzQfooQzB43sGzlpI2At',
+    artist: 'The Goo Goo Dolls',
+    track: 'Dizzy up the Girl',
+    releaseDate: '1998',
+    cover: 'https://i.scdn.co/image/ab67616d00001e02eda9478c39a21e1cdc6609ca'
+});
 </script>
 
 <style scoped>
@@ -26,7 +36,7 @@ import { Head, Link } from '@inertiajs/vue3';
         <div class="bg-white px-8 py-6 shadow-md mt-10 rounded-lg" style="width: 490px;">
 
             <div class="grid grid-cols-1 text-center">
-                <div class="text-gray-600 font-bold">
+                <div class="text-gray-600 font-bold tracking-widest">
                     🎵 NOW PLAYING 🎵
                 </div>
             </div>
@@ -34,9 +44,9 @@ import { Head, Link } from '@inertiajs/vue3';
             <div class="mt-10">
                 <div class="relative">
                     <img 
-                        class="relative rounded"
+                        class="relative rounded drop-shadow-md"
                         style="z-index: 10;"
-                        src="https://i.scdn.co/image/ab67616d00001e02eda9478c39a21e1cdc6609ca"
+                        :src="nowPlaying.cover"
                         alt="cover"
                     >
                     <img 
@@ -48,48 +58,56 @@ import { Head, Link } from '@inertiajs/vue3';
                 </div>
             </div>
 
-            <div style="padding-top: 10px;">
+            <div class="mt-5">
                 <div>
-                    <span class="text-xs text-emerald-400">
+                    <span class="text-xs tracking-wide text-emerald-400">
                         Artist:
                     </span>
-                    <span class="text-gray-400">
-                        The Goo Goo Dolls
+                    <span class="tracking-wide text-gray-400">
+                        {{ nowPlaying.artist }}
                     </span>
                 </div>
                 <div>
-                    <span class="text-xs text-emerald-400">
+                    <span class="text-xs tracking-wide text-emerald-400">
                         Track:
                     </span>
-                    <span class="text-gray-400">
-                        Dizzy up the Girl
+                    <span class="tracking-wide text-gray-400">
+                        {{ nowPlaying.track }}
                     </span>
                 </div>
                 <div>
-                    <span class="text-xs text-emerald-400">
+                    <span class="text-xs tracking-wide text-emerald-400">
                         Release Date:
                     </span>
-                    <span class="text-gray-400">
-                        1998
+                    <span class="tracking-wide text-gray-400">
+                        {{ nowPlaying.releaseDate }}
                     </span>
                 </div>
             </div>
 
-            <div class="mt-10 grid grid-cols-1 text-center">
-                <div class="text-indigo-600">
+            <hr class="mt-10">
+
+            <div class="mt-5 grid grid-cols-1 text-center">
+                <div class="text-indigo-600 tracking-widest">
                     VOTE FOR THIS TRACK
                 </div>
             </div>
 
             <div class="mt-4 grid grid-cols-2 gap-4">
                 <div class="text-right text-2xl">
-                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                    <button 
+                        type="button" 
+                        class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
+                    >
                         Yeah
                         👍
                     </button>
                 </div>
                 <div class="text-left text-2xl">
-                    <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+                    <button 
+                        type="button" 
+                        class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
+                    >
                         Nah
                         👎
                     </button>
