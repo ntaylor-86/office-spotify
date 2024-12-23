c8d91564e9fd0db015067de734872369f60e6ca5
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-23 22:11:31 +1000

    Update TokenController.php

--- app/Http/Controllers/TokenController.php
+++ app/Http/Controllers/TokenController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AccessToken;
+use Carbon\Carbon;
 use SpotifyWebAPI\Session;
 use Illuminate\Http\Request;
 use SpotifyWebAPI\SpotifyWebAPI;
@@ -20,15 +22,25 @@ class TokenController extends Controller
 
     public function index()
     {
-        $session = new Session(env('SPOTIFY_CLIENT_ID'),env('SPOTIFY_CLIENT_SECRET'), 'http://office-spotify.test/test');
+        $session = new Session(env('SPOTIFY_CLIENT_ID'),env('SPOTIFY_CLIENT_SECRET'), route('get-token'));
         $api = new SpotifyWebAPI();
 
         if (isset($_GET['code'])) {
             $session->requestAccessToken($_GET['code']);
             $api->setAccessToken($session->getAccessToken());
 
-            dd($session);
-            dump($api->me());
+            // dd($session);
+            // dump($api->me());
+            $accessToken = new AccessToken();
+            $accessToken->accessToken = $session->getAccessToken();
+            $expirationTime = Carbon::parse($session->getTokenExpiration())->setTimezone('Australia/Brisbane');
+            $accessToken->expirationTime = $expirationTime;
+            $accessToken->redirectUri = $session->getRedirectUri();
+            $accessToken->refreshToken = $session->getRefreshToken();
+            $accessToken->scope = implode(',', $session->getScope());
+            $accessToken->save();
+
+            return redirect()->route('home');
         } else {
             $options = [
                 'scope' => $this->scopes,
