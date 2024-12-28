4c8ed01eebff9587029c26613a6d5b856f553097
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-28 21:17:29 +1000

    Update Home.vue

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -167,14 +167,14 @@ usePoll(10000);
                             class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
                         >
                             Yeah
-                            <img src="images/+1.png" alt="yeah" class="w-6 inline">
+                            <img src="images/+1.png" alt="yeah" class="w-5 inline">
                         </button>
                         <button 
                             type="button" 
                             class="text-gray-700 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 uppercase tracking-widest"
                         >
                             Nah
-                            <img src="images/-1.png" alt="nah" class="w-6 inline">
+                            <img src="images/-1.png" alt="nah" class="w-5 inline">
                         </button>
                     </div>
                 </div>
