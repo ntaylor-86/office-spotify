028e7fe037696d938600f1c7ac432b7de2271316
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-28 20:53:41 +1000

    Adding in the progress percentage

--- app/Spotify/Spotify.php
+++ app/Spotify/Spotify.php
@@ -18,6 +18,8 @@ class Spotify
             $api->setAccessToken($accessToken);
             $currentTrack = $api->getMyCurrentTrack();
 
+            // dd($currentTrack);
+
             if (is_null($currentTrack)) {
                 return [
                     'data' => null
@@ -31,13 +33,19 @@ class Spotify
                 $releaseDate = $matches[0];
             }
 
+            // Working out the progress of the track from the Spofity API
+            $totalLength = $currentTrack->item->duration_ms;
+            $progress = $currentTrack->progress_ms;
+            $progressPercentage = round( ( ($progress * 100) / $totalLength ), 2 );
+
             return [
                 'data' => [
                     'uri' => $currentTrack->item->uri,
                     'artist' => $currentTrack->item->artists[0]->name,
                     'track' => $currentTrack->item->name,
                     'releaseDate' => $releaseDate,
-                    'cover' => $currentTrack->item->album->images[0]->url
+                    'cover' => $currentTrack->item->album->images[0]->url,
+                    'progress' => $progressPercentage
                 ]
             ];
         } catch (\Throwable $th) {
