e72a47663bef42dc04025ec7dfeea53776421946
Nathan Taylor <ntaylor0377@gmail.com>
2024-12-30 21:41:42 +1000

    added docbloc to AccessToken model

--- app/Models/AccessToken.php
+++ app/Models/AccessToken.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class AccessToken extends Model
 {
+    /**
+     * Get the attributes that should be cast.
+     * 
+     * @return array<string, string>
+     */
     protected function casts(): array
     {
         return [
